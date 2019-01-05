59e7c767fb67980e65980b494f87f033b9fc473a
Noob-X <ajnatakaundinya@gmail.com>
2019-01-06 01:28:35 +0800

    Optimize.

--- FastIntMath/fast_int_math_v2.hpp
+++ FastIntMath/fast_int_math_v2.hpp
@@ -2,13 +2,14 @@
 
 #include <stdint.h>
 
-__device__ __forceinline__ uint32_t get_reciprocal(uint32_t a)
+__device__ __forceinline__ uint32_t get_reciprocal(const uint32_t a)
 {
 	const float a_hi = __uint_as_float((a >> 8) + ((126U + 31U) << 23));
 	const float a_lo = __uint2float_rn(a & 0xFF);
 
 	float r;
-	asm("rcp.approx.f32 %0, %1;" : "=f"(r) : "f"(a_hi));
+	asm("rcp.f32.rn %0, %1;" : "=f"(r) : "f"(a_hi));
+
 	const float r_scaled = __uint_as_float(__float_as_uint(r) + (64U << 23));
 
 	const float h = __fmaf_rn(a_lo, r, __fmaf_rn(a_hi, r, -1.0f));
@@ -51,7 +52,7 @@ __device__ __forceinline__ uint64_t get_reciprocal64(uint32_t a)
 	return result;
 }
 
-__device__ __forceinline__ uint64_t fast_div_v2(uint64_t a, uint32_t b)
+__device__ __forceinline__ uint64_t fast_div_v2(const uint64_t a, const uint32_t b)
 {
 	const uint32_t r = get_reciprocal(b);
 	const uint64_t k = __umulhi(((uint32_t*)&a)[0], r) + ((uint64_t)(r) * ((uint32_t*)&a)[1]) + a;
@@ -62,11 +63,11 @@ __device__ __forceinline__ uint64_t fast_div_v2(uint64_t a, uint32_t b)
 	int64_t tmp = a - (uint64_t)(q[0]) * b;
 	((int32_t*)(&tmp))[1] -= (k < a) ? b : 0;
 
-	const bool overshoot = ((int32_t*)(&tmp))[1] < 0;
-	const bool undershoot = tmp >= b;
+	const int8_t overshoot = ((int32_t*)(&tmp))[1] >> 31;
+	const int8_t undershoot = (tmp - b) >> 63;
 
-	q[0] += (undershoot ? 1U : 0U) - (overshoot ? 1U : 0U);
-	q[1] = ((uint32_t*)(&tmp))[0] + (overshoot ? b : 0U) - (undershoot ? b : 0U);
+	q[0] += 1 + undershoot + overshoot;
+	q[1] = ((uint32_t*)(&tmp))[0] + (b & overshoot) + ((b & undershoot) - b);
 
 	return *((uint64_t*)(q));
 }
@@ -76,7 +77,7 @@ __device__ __forceinline__ uint32_t fast_sqrt_v2(const uint64_t n1)
 	float x = __uint_as_float((((uint32_t*)&n1)[1] >> 9) + ((64U + 127U) << 23));
 	float x1;
 	asm("rsqrt.approx.f32 %0, %1;" : "=f"(x1) : "f"(x));
-	asm("sqrt.approx.f32 %0, %1;" : "=f"(x) : "f"(x));
+	asm("rcp.approx.f32 %0, %1;" : "=f"(x) : "f"(x1));
 
 	// The following line does x1 *= 4294967296.0f;
 	x1 = __uint_as_float(__float_as_uint(x1) + (32U << 23));
@@ -90,8 +91,7 @@ __device__ __forceinline__ uint32_t fast_sqrt_v2(const uint64_t n1)
 	const uint32_t b = result & 1;
 
 	const uint64_t x2 = (uint64_t)(s) * (s + b) + ((uint64_t)(result) << 32) - n1;
-	if ((int64_t)(x2 + b) > 0) --result;
-	if ((int64_t)(x2 + 0x100000000UL + s) < 0) ++result;
+	result -= (((int64_t)(x2 + b) >> 63) + 1) + ((int64_t)(x2 + 0x100000000UL + s) >> 63);
 
 	return result;
 }
